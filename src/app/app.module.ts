cd04f7dbdb5c30291d231457dcd6c9f3e9dd7853
Pedro <pedro.arruda@outlook.com.br>
2020-08-18 12:23:54 -0300

    Implementacao da utilizacao de dropdown

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { PerfilComponent } from './perfil/perfil.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { NavComponent } from './nav/nav.component';
 import { TituloComponent } from './titulo/titulo.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 @NgModule({
   declarations: [
@@ -22,7 +24,9 @@ import { TituloComponent } from './titulo/titulo.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    BsDropdownModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/perfil/perfil.component.ts
+++ src/app/perfil/perfil.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerfilComponent implements OnInit {
 
-  public titulo = 'Professores';
+  public titulo = 'Perfil';
   constructor() { }
 
   ngOnInit(): void {

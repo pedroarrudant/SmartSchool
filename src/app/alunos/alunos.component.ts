52f6e0db05ca86df9e5993b3d28052746d108bb2
Pedro <pedro.arruda@outlook.com.br>
2020-08-19 18:43:13 -0300

    Introducao Reactive Forms

--- src/app/alunos/alunos.component.ts
+++ src/app/alunos/alunos.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Aluno } from '../models/Aluno';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-alunos',
@@ -8,28 +9,48 @@ import { Aluno } from '../models/Aluno';
 })
 export class AlunosComponent implements OnInit {
 
+  public alunoForm: FormGroup;
   AlunoSelecionado: Aluno;
 
   titulo = 'Alunos';
   public alunos = [
-    {id: 1, nome: 'Aluno1', sobrenome: 'Sobrenome1'},
-    {id: 2, nome: 'Aluno2', sobrenome: 'Sobrenome2'},
-    {id: 3, nome: 'Aluno3', sobrenome: 'Sobrenome3'},
-    {id: 4, nome: 'Aluno4', sobrenome: 'Sobrenome4'},
-    {id: 5, nome: 'Aluno5', sobrenome: 'Sobrenome5'},
-    {id: 6, nome: 'Aluno6', sobrenome: 'Sobrenome6'},
+    {id: 1, nome: 'Aluno1', sobrenome: 'Sobrenome1', telefone: '12345678'},
+    {id: 2, nome: 'Aluno2', sobrenome: 'Sobrenome2', telefone: '12345678'},
+    {id: 3, nome: 'Aluno3', sobrenome: 'Sobrenome3', telefone: '12345678'},
+    {id: 4, nome: 'Aluno4', sobrenome: 'Sobrenome4', telefone: '12345678'},
+    {id: 5, nome: 'Aluno5', sobrenome: 'Sobrenome5', telefone: '12345678'},
+    {id: 6, nome: 'Aluno6', sobrenome: 'Sobrenome6', telefone: '12345678'},
   ];
+
+  constructor(private fb: FormBuilder) {
+    this.criarForm();
+   }
+
+  ngOnInit(): void {
+  }
+
+  criarForm(){
+    this.alunoForm = this.fb.group({
+      nome: [''],
+      sobrenome: [''],
+      telefone: ['']
+    });
+  }
+
+  alunoSubmit(){
+    // var aluno = {id: 1, nome: 'Pedro', sobrenome: 'Arruda', telefone: '12345678'};
+    // this.alunoForm.get('telefone')
+    // this.alunos.push(aluno);
+    console.log(this.alunoForm.value);
+    // console.log(this.alunos);
+  }
 
   SelecaoAluno(aluno:Aluno){
     this.AlunoSelecionado = aluno;
+    this.alunoForm.patchValue(aluno);
   }
 
   DeselecionarAluno(){
     this.AlunoSelecionado = null;
   }
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
 }
